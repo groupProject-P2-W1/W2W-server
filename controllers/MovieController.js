4d2ff0bdd686020a6abd0f16df053b55917f9272
erick <erick.gunawan70@gmail.com>
2020-11-27 14:42:23 +0700

    fixed

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -69,6 +69,17 @@ class MovieController {
             })
         })
     }
+    static list (req, res, next){
+        Watchlist.findAll({where: {UserId: req.loggedInUser.id}})
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Internal Server Error'
+            })
+        })
+    }
 }
 
 module.exports = MovieController
