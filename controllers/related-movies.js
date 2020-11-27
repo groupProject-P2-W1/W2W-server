36cfa107879872b8f9a3a1c8ba6475217699368e
adhim1st <adhim@student.uns.ac.id>
2020-11-27 13:50:11 +0700

    fix routes

--- controllers/related-movies.js
+++ controllers/related-movies.js
@@ -4,7 +4,7 @@ class RelatedMovies {
   static getSimilarMovies(req, res, next) {
     let moviequery = req.params.title; // untuk detail per movie nya bawa parameter judul movie
     axios({
-      url: `https://tastedive.com/api/similar?info=1&q=${moviequery}&k=${process.env.JWT_SECRET}`,
+      url: `https://tastedive.com/api/similar?info=1&q=${moviequery}&k=${process.env.TASTEDIVE_API_KEY}`,
       method: "GET",
     })
       .then((data) => {
--- routes/movie.js
+++ routes/movie.js
@@ -1,10 +1,12 @@
-const router = require("express").Router()
-const authenticate = require("../middlewares/authentication")
-const MovieController = require("../controllers/MovieController")
+const router = require("express").Router();
+const authenticate = require("../middlewares/authentication");
+const MovieController = require("../controllers/MovieController");
+const RelatedMovies = require("../controllers/related-movies");
 
-router.use(authenticate)
-router.get("/popular", MovieController.getPopularMovies)
-router.post("/watchlist", MovieController.addWatchlist)
-router.delete("/watchlist/:id", MovieController.deleteWatchlist)
+router.use(authenticate);
+router.get("/popular", MovieController.getPopularMovies);
+router.post("/watchlist", MovieController.addWatchlist);
+router.delete("/watchlist/:id", MovieController.deleteWatchlist);
+router.get("/similarmovies/:title", RelatedMovies.getSimilarMovies);
 
-module.exports = router
+module.exports = router;
